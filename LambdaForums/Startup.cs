f8dd1caa899e8e9fcc6eb273d252a90d94ce38c1
vitaliy shkuratov <vitalij.shkuratov@gmail.com>
2019-05-15 21:53:45 -0400

    Initial commit, create Data Project

--- LambdaForums/Startup.cs
+++ LambdaForums/Startup.cs
@@ -38,11 +38,13 @@ namespace LambdaForums
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
